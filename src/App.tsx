a88affc707929a64249c84469b338a01ada1630b
xo-azeem <mailmeatazeem@gmail.com>
2025-07-02 00:17:21 +0500

    fix: navlink

--- src/App.tsx
+++ src/App.tsx
@@ -345,7 +345,7 @@ function App() {
         </section>
 
         {/* Features Section */}
-        <section className="py-20 sm:py-24 bg-gray-900">
+        <section id='about' className="py-20 sm:py-24 bg-gray-900">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12">
               {features.map((feature, index) => {
